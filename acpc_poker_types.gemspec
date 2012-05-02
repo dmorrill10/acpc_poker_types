fefbdfa700849200feb7b35db71fbed94e752901
Dustin Morrill <morrill@ualberta.ca>
2012-05-01 20:42:05 -0600

    Forgot to fix some other paths.

--- acpc_poker_types.gemspec
+++ acpc_poker_types.gemspec
@@ -20,7 +20,7 @@ Gem::Specification.new do |s|
   
   s.rubyforge_project = "acpc_poker_types"
 
-  s.files         = Dir.glob("lib/**/*") + Dir.glob("src/**/*") + Dir.glob("ext/**/*") + Dir.glob("external/**/*") + %w(Rakefile acpc_poker_types.gemspec tasks.rb README.md)
+  s.files         = Dir.glob("lib/**/*") + Dir.glob("ext/**/*") + Dir.glob("external/**/*") + %w(Rakefile acpc_poker_types.gemspec tasks.rb README.md)
   s.test_files    = Dir.glob "spec/**/*"
   s.extensions    = FileList["ext/**/extconf.rb"]
   s.require_paths = ["lib"]
--- lib/acpc_poker_types.rb
+++ lib/acpc_poker_types.rb
@@ -1,18 +1,18 @@
 require "version"
 
-require File.expand_path("../../src/acpc_poker_types_defs", __FILE__)
-require File.expand_path("../../src/types/board_cards", __FILE__)
-require File.expand_path("../../src/types/card", __FILE__)
-require File.expand_path("../../src/types/chip_stack", __FILE__)
-require File.expand_path("../../src/types/game_definition", __FILE__)
-require File.expand_path("../../src/types/hand", __FILE__)
-require File.expand_path("../../src/types/match_state_string", __FILE__)
-require File.expand_path("../../src/types/pile_of_cards", __FILE__)
-require File.expand_path("../../src/types/player", __FILE__)
-require File.expand_path("../../src/types/pot", __FILE__)
-require File.expand_path("../../src/types/rank", __FILE__)
-require File.expand_path("../../src/types/side_pot", __FILE__)
-require File.expand_path("../../src/types/suit", __FILE__)
+require File.expand_path("../acpc_poker_types/acpc_poker_types_defs", __FILE__)
+require File.expand_path("../acpc_poker_types/types/board_cards", __FILE__)
+require File.expand_path("../acpc_poker_types/types/card", __FILE__)
+require File.expand_path("../acpc_poker_types/types/chip_stack", __FILE__)
+require File.expand_path("../acpc_poker_types/types/game_definition", __FILE__)
+require File.expand_path("../acpc_poker_types/types/hand", __FILE__)
+require File.expand_path("../acpc_poker_types/types/match_state_string", __FILE__)
+require File.expand_path("../acpc_poker_types/types/pile_of_cards", __FILE__)
+require File.expand_path("../acpc_poker_types/types/player", __FILE__)
+require File.expand_path("../acpc_poker_types/types/pot", __FILE__)
+require File.expand_path("../acpc_poker_types/types/rank", __FILE__)
+require File.expand_path("../acpc_poker_types/types/side_pot", __FILE__)
+require File.expand_path("../acpc_poker_types/types/suit", __FILE__)
 
 module AcpcPokerTypes
 end
