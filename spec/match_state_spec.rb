a909cede44b799fd65450964537ae95e709bd2af
Dustin Morrill <dmorrill10@gmail.com>
2013-08-15 13:23:38 -0600

    Added test.

--- spec/match_state_spec.rb
+++ spec/match_state_spec.rb
@@ -658,6 +658,25 @@ describe MatchState do
           end
         end
       end
+      it 'when players are all-in' do
+        game_def = GameDefinition.new(
+          :betting_type=>"nolimit",
+          :chip_stacks=>[20000, 20000],
+          :number_of_players=>2,
+          :blinds=>[100, 50],
+          :raise_sizes=>nil,
+          :number_of_rounds=>4,
+          :first_player_positions=>[1, 0, 0, 0],
+          :max_number_of_wagers=>[255],
+          :number_of_suits=>4,
+          :number_of_ranks=>13,
+          :number_of_hole_cards=>2,
+          :number_of_board_cards=>[0, 3, 1, 1]
+        )
+        MatchState.parse(
+          'MATCHSTATE:0:2:cr20000c///:8h8s|5s5c/KdTcKh/9h/Jh'
+        ).players(game_def).map { |pl| pl.winnings }.must_equal [40000, 0]
+      end
     end
   end
   describe '#pot' do
